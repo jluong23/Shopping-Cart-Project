9b30d26242d9a17395641f1e0c59d90bdde5252c
James Luong <jamesluong@hotmail.co.uk>
2022-07-11 21:50:36 +0100

    Fixed descriptions of some products, improved positioning of page content class

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import DailySong from "./pages/DailySong";
 import React, { useState } from 'react';
 import Footer from "./components/Footer";
 import Product from "./pages/Product";
+import Checkout from "./pages/Checkout";
 
 const App = () => {
   const [navBarVisible, setNavBarVisible] = useState(true);
@@ -15,16 +16,17 @@ const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
     {navBarVisible ? <Navbar /> : null} 
-      <div className="page-content">
-        <Routes>
-          <Route path="/" 
-            element={<Home setNavBarVisible={setNavBarVisible} setFooterVisible = {setFooterVisible} />}/>
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/dailysong" element={<DailySong />} />
-          <Route path="/product" element ={<Product />} />
-        </Routes>
-      </div>
-      {footerVisible ? <Footer /> : null}
+    <div className="page-content">
+      <Routes>
+        <Route path="/" 
+          element={<Home setNavBarVisible={setNavBarVisible} setFooterVisible = {setFooterVisible} />}/>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/dailysong" element={<DailySong />} />
+        <Route path="/product" element ={<Product />} />
+        <Route path="/checkout" element ={<Checkout />} />
+      </Routes>
+    </div>
+    {footerVisible ? <Footer /> : null}
       
 
     </BrowserRouter>
---
+++ src/pages/Checkout.js
@@ -0,0 +1,15 @@
+// Checkout.js
+
+import React, { Component } from "react";
+import productData from "../products.json";
+
+const Checkout = (props) => {
+
+    return (
+        <div>
+            <h1>Checkout</h1>
+        </div>
+    );
+  };
+  
+export default Checkout;
